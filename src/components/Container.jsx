d0d57df05fa254a345e2107707ed6c563b285330
Jakub Šmíd <jakubsmid22@gmail.com>
2025-01-29 15:28:00 +0100

    Update Container component layout for improved spacing

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,12 +3,12 @@ import starIcon from "../assets/images/icon-star.svg";
 import Item from "./Item";
 const Container = () => {
   return (
-    <div className="font-work-sans w-[90%] md:w-[600px] h-fit bg-white z-20 p-7 rounded-xl flex flex-col gap-5">
+    <div className="font-work-sans w-[90%] md:w-[600px] h-fit bg-white z-20 p-7 rounded-xl flex flex-col gap-7">
       <div className="flex items-center gap-5">
         <img src={starIcon} alt="star-icon" />
         <h1 className="font-bold text-5xl">FAQs</h1>
       </div>
-      <div className="space-y-7 md:space-y-4">
+      <div className="space-y-7">
         {accordions.map((item, index) => (
           <>
             <Item
